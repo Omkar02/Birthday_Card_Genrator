9bc7343cd8823481b5723d88449bd1b4a1bb37d9
Omkar Joshi <omkarjoshi4031@live.com>
2020-01-18 22:05:13 +0530

    Add files via upload

---
+++ TEXT_WRITER.py
@@ -0,0 +1,53 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from helper import quotes
+import random
+import os
+import csv
+
+
+
+
+def Card_Prep(person_name,card_no,quote,font_path):
+    img = Image.open("All_cards/"+str(card_no)+".jpg")
+    draw = ImageDraw.Draw(img)
+
+    font = ImageFont.truetype(font_path, 76)
+    draw.text((100, 2170), f'Dear {person_name},\n{quote}\n     '
+                           f'                                -From GPTW Team.',(0, 0, 0), font=font)
+    img.save(f'output/{person_name}.jpg')
+    print(f'{person_name} saved!')
+
+
+
+
+
+path, dirs, files = next(os.walk("All_cards"))
+file_count = len(files)
+
+
+
+path1, dirs1, files1 = next(os.walk("All_font"))
+file_count1 = len(files1)
+# rand_font = random.randint(0,file_count1)
+
+
+with open("names.txt", "r") as f:
+    all_name = [row for row in csv.reader(f,delimiter=',')]
+
+
+
+for names in all_name[0]:
+    rand_quotes = random.randint(1, len(quotes))
+    rand_card = random.randint(1, file_count)
+    rand_font = random.randint(0, file_count1-1)
+    # rand_font = 'All_font/Great Wishes.otf'
+    # print(names)
+    print(names, rand_card, rand_quotes, files1[rand_font])
+    Card_Prep(names, rand_card, quotes[rand_quotes], path1 + '/' + files1[rand_font])
+    # print(names, rand_card, rand_quotes, rand_font)
+    # Card_Prep(str(names), rand_card, quotes[rand_quotes], rand_font)
+    print(f'Done for {names}')
+#
+
---
+++ helper.py
@@ -0,0 +1,12 @@
+quotes = {
+    1:'Count your life by smiles, not tears.\nCount your age by friends, not years. \nHappy birthday!',
+    2:'May the joy that you have spread in the past\ncome back to you on this day.\nWishing you a very happy birthday',
+    3:'Wishing you a day filled with happiness and\na year filled with joy. \nHappy birthday!',
+    4:'Sending you smiles for every moment of your\nspecial day…Have a wonderful time and a\nvery happy birthday!',
+    5:'Sending your way a bouquet of happiness…\nTo wish you a \nvery happy birthday!',
+    6:'Wishing you a beautiful day with\ngood health and happiness forever.\nHappy birthday!'
+}
+
+font_size_sort = {
+
+}
